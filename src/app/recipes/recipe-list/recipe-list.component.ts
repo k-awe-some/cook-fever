126edf120b4d9436658108c4f0d2aeeb6713705c
kay <kaynguyen.dev@gmail.com>
2020-02-02 18:22:15 -0500

    dynamically display recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,13 +12,13 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
       "Test Recipe 1",
-      "This is a test recipe",
+      "This is Test Recipe 1.",
       "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg"
     ),
 
     new Recipe(
       "Test Recipe 2",
-      "This is a test recipe",
+      "This is Test Recipe 2.",
       "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg"
     )
   ];

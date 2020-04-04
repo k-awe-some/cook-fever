ca8936224b90fba24e6ed3145268fb7fc4b7c485
Kay N <kaynguyen.dev@gmail.com>
2020-04-04 18:01:40 -0400

    shopping list service - addIngredient() & getIngredients()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
-import { DropdownDirective } from './shared/dropdown.directive';
+import { DropdownDirective } from "./shared/dropdown.directive";
 
 @NgModule({
   declarations: [
@@ -21,10 +22,10 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ShoppingListService],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}

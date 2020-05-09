72ccf472d28d6346b43297eb6d7e457ef35e65ec
Kay N <kaynguyen.dev@gmail.com>
2020-05-09 13:49:00 -0400

    set up routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+/* Modules */
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app.routing.module";
+/* Components */
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
+/* Services and Directives */
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { DropdownDirective } from "./shared/dropdown.directive";
 
 @NgModule({
@@ -24,7 +28,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })

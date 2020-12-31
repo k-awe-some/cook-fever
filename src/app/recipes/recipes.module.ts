916c343fff065d46194d77514ca7e3f7896f2796
Kay N <kaynguyen.dev@gmail.com>
2020-12-31 11:17:46 -0500

    removed redundant imports of routed components

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -20,13 +20,5 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     RecipeEditComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule, RecipesRoutingModule],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
-  ],
 })
 export class RecipesModule {}

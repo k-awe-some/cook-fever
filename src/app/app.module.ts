40342cc29fae0ea3b269a46a8c30d113d8057b37
Kay N <kaynguyen.dev@gmail.com>
2021-01-07 08:50:41 -0500

    installed ngrx store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { AppRoutingModule } from "./app.routing.module";
 import { SharedModule } from "./shared/shared.module";
@@ -13,6 +14,7 @@ import { HeaderComponent } from "./header/header.component";
 
 import { appReducer } from "./store/store.reducer";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -22,6 +24,7 @@ import { AuthEffects } from "./auth/store/auth.effects";
     AppRoutingModule,
     StoreModule.forRoot(appReducer), // register reducers
     EffectsModule.forRoot([AuthEffects]), // register effects
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     SharedModule,
     CoreModule,
   ],

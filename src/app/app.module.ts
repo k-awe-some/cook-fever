6cbd6be8bf77c12c5934678cee4989ca782cc8f8
Kay N <kaynguyen.dev@gmail.com>
2021-01-07 08:54:30 -0500

    installed ngrx router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from "@angular/core";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
 import { AppRoutingModule } from "./app.routing.module";
 import { SharedModule } from "./shared/shared.module";
@@ -25,6 +26,7 @@ import { environment } from "src/environments/environment";
     StoreModule.forRoot(appReducer), // register reducers
     EffectsModule.forRoot([AuthEffects]), // register effects
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule,
   ],

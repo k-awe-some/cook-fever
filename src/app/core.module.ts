e8e496d2f32473ce270cb612430195b5651f87ca
Kay N <kaynguyen.dev@gmail.com>
2021-02-15 16:14:10 -0500

    cleaned up

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,11 +2,9 @@ import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
-import { RecipesService } from "./recipes/recipes.service";
 
 @NgModule({
   providers: [
-    RecipesService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -10,29 +10,17 @@ import { of } from "rxjs";
 import { map, switchMap, take } from "rxjs/operators";
 
 import { Recipe } from "./recipe.model";
-import { DataStorageService } from "../shared/data-storage.service";
-import { RecipesService } from "./recipes.service";
 import * as StoreType from "../store/store.model";
 import * as RecipeActions from "../recipes/store/recipe.actions";
 
 @Injectable({ providedIn: "root" })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(
-    private dataStorageService: DataStorageService,
-    private recipesService: RecipesService,
     private store: Store<StoreType.IStore>,
     private actions$: Actions
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    // const recipes = this.recipesService.getRecipes();
-
-    // only fetch recipes on first load
-    // to protect locally saved data
-    // return recipes.length === 0
-    //   ? this.dataStorageService.fetchRecipes()
-    //   : recipes;
-
     return this.store.select("recipes").pipe(
       take(1),
       map((recipesState) => {
@@ -47,6 +35,5 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
         }
       })
     );
-    this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 }
